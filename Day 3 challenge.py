b51bda90e79b999932513fb3d2ef65d2c4474395
sandhya972 <sandhyayerragudi@gmail.com>
2026-02-10 20:16:14 +0530

    Add files via upload

---
+++ Day 3 challenge.py
@@ -0,0 +1,28 @@
+n=int(input("Enter the number of students:"))
+marks=[]
+count_valid=0
+count_failed=0
+for i in range(n):
+    m=int(input("Enter marks:"))
+    marks.append(m)
+for m in marks:
+    if m>=90 and m<=100:
+        print(m ,"->Excellent")
+        count_valid = count_valid + 1
+    elif m>=75 and m<=89:
+        print(m,"->Very Good")
+        count_valid = count_valid + 1
+    elif m>=60 and m<=74:
+        print(m,"->Good")
+        count_valid = count_valid + 1
+    elif m>=40 and m<=59:
+        print(m,"->Average")
+        count_valid = count_valid + 1
+    elif m>=0 and m<=39:
+        print(m,"->Fail")
+        count_valid = count_valid + 1
+        count_failed = count_failed + 1
+    else:
+        print(m,"->Invalid")
+print("Total valid Students:",count_valid)
+print("Total failed Students:",count_failed)
